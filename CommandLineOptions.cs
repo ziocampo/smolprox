aba7ff72a34a667009c7569b13957acb55e215bf
Daniele Bruni <daniele.bruni@tecnike.net>
2022-08-09 16:48:08 +0200

    initial commit

---
+++ CommandLineOptions.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+internal class CommandLineOptions
+{
+    
+    [Option('p', "port", Required = false, Default = 5200, HelpText = "Port to proxy")]
+    public int Port { get; set; }
+
+    [Option('h', "host", Required = false, Default = "localhost", HelpText = "Hostname or IP to proxy")]
+    public string Host { get; set; }
+
+    [Option('s', "https", Required = false, Default = false, HelpText = "Is https?")]
+    public bool Https { get; set; }
+
+    [Option('l', "listen", Required = false, Default = 5200, HelpText = "Port to listen.")]
+    public int Listen { get; set; }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using CommandLine;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
+using Yarp.ReverseProxy.Forwarder;
+using NLog.Web;
+
+Parser
+  .Default
+  .ParseArguments<CommandLineOptions>(args)
+  .WithParsed(o =>
+  {
+
+      var webhost = WebHost
+                    .CreateDefaultBuilder()
+                    .UseKestrel(
+                        options =>
+                        {
+                            options.AddServerHeader = false;
+                            options.ListenAnyIP(o.Listen);
+                            options.AllowSynchronousIO = true;
+                        }
+                    )
+                    .ConfigureServices((IServiceCollection services) => {
+                        services.AddHttpContextAccessor();
+                        services.AddHttpLogging(options => {
+                            options.LoggingFields = HttpLoggingFields.All;
+                        });
+                        services.AddHttpForwarder();
+                    })
+                    .Configure((IApplicationBuilder app) => {
+
+                        IHttpForwarder forwarder = app.ApplicationServices.GetRequiredService<IHttpForwarder>();
+
+                        app.UseRouting();
+
+                        app.UseHttpLogging();
+                        app.UseForwardedHeaders(new ForwardedHeadersOptions
+                        {
+                            ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                        });
+
+                        var httpClient = new HttpMessageInvoker(new SocketsHttpHandler()
+                        {
+                            UseProxy = false,
+                            AllowAutoRedirect = false,
+                            AutomaticDecompression = DecompressionMethods.None,
+                            UseCookies = false
+                        });
+                        var requestOptions = new ForwarderRequestConfig { ActivityTimeout = TimeSpan.FromSeconds(100) };
+                        var remoteBaseUri = $"http{(o.Https ? "s": string.Empty)}://{o.Host}:{o.Port}";
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.Map($"/{{**catch-all}}", async httpContext =>
+                            {
+                                var error = await forwarder.SendAsync(httpContext, remoteBaseUri, httpClient, requestOptions);
+                                if (error != ForwarderError.None)
+                                {
+                                    var errorFeature = httpContext.Features.Get<IForwarderErrorFeature>();
+                                    var exception = errorFeature.Exception;
+                                }
+                            });
+
+                        });
+                    })
+                    .UseNLog()
+                    .Build();
+
+      webhost.Run();
+
+  });
+
